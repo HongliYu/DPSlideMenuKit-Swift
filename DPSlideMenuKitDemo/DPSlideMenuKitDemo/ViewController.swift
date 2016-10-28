23657d9033b9574ba44b86d3ed09f3472723a007
HongliYu <yhlssdone@gmail.com>
2016-10-28 22:37:14 +0800

    update README.md

--- DPSlideMenuKitDemo/DPSlideMenuKitDemo/ViewController.swift
+++ DPSlideMenuKitDemo/DPSlideMenuKitDemo/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    // 1. get the drawer
     // embed in storyboard
     var drawer: DPDrawerViewController?
     self.childViewControllers.forEach { (viewController) in
@@ -23,34 +24,34 @@ class ViewController: UIViewController {
       }
     }
     // not embed in storyboard? add it manually
-//    let drawer: DPDrawerViewController? = self.storyboard?.instantiateViewController(withIdentifier: "DPDrawerViewController") as? DPDrawerViewController
-//    self.addChildViewController(drawer!)
-//    self.view.addSubview(drawer!.view)
-
+    // let drawer: DPDrawerViewController? = self.storyboard?.instantiateViewController(withIdentifier: "DPDrawerViewController") as? DPDrawerViewController
+    //  self.addChildViewController(drawer!)
+    //  self.view.addSubview(drawer!.view)
+    
+    // 2. create the first view controller
     let homeViewController: DPHomeViewController? = self.storyboard?.instantiateViewController(withIdentifier: "DPHomeViewController") as? DPHomeViewController
-    let slideMenuModelProjects: DPSlideMenuModel = DPSlideMenuModel.init(
-      color: UIColor.init(colorLiteralRed: 237.0 / 255.0,
-        green: 140.0 / 255.0,
-        blue: 52.0 / 255.0,
-        alpha: 1.0),
+    
+    // 3. create DPSlideMenuModel for every view controller
+    let slideMenuModelProjects: DPSlideMenuModel = DPSlideMenuModel(
+      color: UIColor(colorLiteralRed: 237.0 / 255.0, green: 140.0 / 255.0,
+                     blue: 52.0 / 255.0,
+                     alpha: 1.0),
       controllerClassName: "DPHomeViewController",
       title: "Projects",
       cellHeight: 108.0,
       actionBlock: nil)
-    let slideMenuModelSupport: DPSlideMenuModel = DPSlideMenuModel.init(
-      color: UIColor.init(colorLiteralRed: 140.0 / 255.0,
-        green: 155.0 / 255.0,
-        blue: 237.0 / 255.0,
-        alpha: 1.0),
+    let slideMenuModelSupport: DPSlideMenuModel = DPSlideMenuModel(
+      color: UIColor(colorLiteralRed: 140.0 / 255.0, green: 155.0 / 255.0,
+                     blue: 237.0 / 255.0,
+                     alpha: 1.0),
       controllerClassName: "DPSupportViewController",
       title: "Support",
       cellHeight: 88.0,
       actionBlock: nil)
-    let slideMenuModelRate: DPSlideMenuModel = DPSlideMenuModel.init(
-      color: UIColor.init(colorLiteralRed: 237.0 / 255.0,
-        green: 140 / 255.0,
-        blue: 200.0 / 255.0,
-        alpha: 1.0),
+    let slideMenuModelRate: DPSlideMenuModel = DPSlideMenuModel(
+      color: UIColor(colorLiteralRed: 237.0 / 255.0, green: 140 / 255.0,
+                     blue: 200.0 / 255.0,
+                     alpha: 1.0),
       controllerClassName: nil,
       title: "Rate",
       cellHeight: 88.0,
@@ -58,11 +59,10 @@ class ViewController: UIViewController {
         let urlString: String = "itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewSoftware?id=910117892" // replace 910117892 with your appid
         UIApplication.shared.openURL(URL(string: urlString)!)
     })
-    let slideMenuModelDonate: DPSlideMenuModel = DPSlideMenuModel.init(
-      color: UIColor.init(colorLiteralRed: 237.0 / 255.0,
-        green: 100.0 / 255.0,
-        blue: 100.0 / 255.0,
-        alpha: 1.0),
+    let slideMenuModelDonate: DPSlideMenuModel = DPSlideMenuModel(
+      color: UIColor(colorLiteralRed: 237.0 / 255.0, green: 100.0 / 255.0,
+                     blue: 100.0 / 255.0,
+                     alpha: 1.0),
       controllerClassName: nil,
       title: "Donate",
       cellHeight: 88.0,
@@ -72,7 +72,9 @@ class ViewController: UIViewController {
     })
     let slideMenuModels: [DPSlideMenuModel] = [slideMenuModelProjects, slideMenuModelSupport,
                                                slideMenuModelRate, slideMenuModelDonate]
-    let leftMenuViewController: DPLeftMenuViewController = DPLeftMenuViewController.init(slideMenuModels: slideMenuModels, storyboard: self.storyboard)
+    
+    // 4. create leftMenuViewController with DPSlideMenuModel array, then reset the drawer
+    let leftMenuViewController: DPLeftMenuViewController = DPLeftMenuViewController(slideMenuModels: slideMenuModels, storyboard: self.storyboard)
     drawer?.reset(leftViewController: leftMenuViewController,
                   centerViewController: homeViewController)
   }
