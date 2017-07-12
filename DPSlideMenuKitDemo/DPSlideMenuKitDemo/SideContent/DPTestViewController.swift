33258583007488707504ac029958ff94e10b4748
HongliYu <yhlssdone@gmail.com>
2017-07-12 18:12:36 +0800

    1. generate vc from code bug fixed. 2.refactor the api 3. update the kit files for framework

---
+++ DPSlideMenuKitDemo/DPSlideMenuKitDemo/SideContent/DPTestViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DPTestViewController.swift
+//  DPSlideMenuKitDemo
+//
+//  Created by Hongli Yu on 12/07/2017.
+//  Copyright © 2017 Hongli Yu. All rights reserved.
+//
+
+import UIKit
+
+// This view controller is not in the storyboard, but can be added in the left menu as well.
+class DPTestViewController: DPBaseEmbedViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.view.backgroundColor = UIColor.red
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+  
+}
--- DPSlideMenuKitDemo/DPSlideMenuKitDemo/ViewController.swift
+++ DPSlideMenuKitDemo/DPSlideMenuKitDemo/ViewController.swift
@@ -23,27 +23,27 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     
     // 1. not embed in storyboard? add it manually
-    // let drawer: DPDrawerViewController? = self.storyboard?.instantiateViewController(withIdentifier: "DPDrawerViewController") as? DPDrawerViewController
-    //  self.addChildViewController(drawer!)
-    //  self.view.addSubview(drawer!.view)
-    
+//   let drawer: DPDrawerViewController? = self.storyboard?.instantiateViewController(withIdentifier: "DPDrawerViewController") as? DPDrawerViewController
+//    self.addChildViewController(drawer!)
+//    self.view.addSubview(drawer!.view)
+//    DPSlideMenuManager.shared.setDrawer(drawer: drawer)
+
     let leftMenuViewControllerNameArray: [String] = ["DPTeamViewController",
                                                      "DPChannelListViewController",
                                                      "DPMessageListViewController"]
     let leftMenuViewControllers: [DPBaseEmbedViewController] = UIViewController.generateViewControllersFrom(viewControllerNameArray: leftMenuViewControllerNameArray, storyboardName: "Main", bundle: nil) as! [DPBaseEmbedViewController]
     
+    // 2. not from story board, add it manually
+//    let leftMenuViewControllerNameArrayFromCode: [String] = ["DPTestViewController"]
+//    let leftMenuViewControllersFromCode: [DPBaseEmbedViewController] = UIViewController.generateViewControllersFrom(viewControllerNameArray: leftMenuViewControllerNameArrayFromCode, storyboardName: nil, bundle: nil) as! [DPBaseEmbedViewController]
+//    leftMenuViewControllers.append(contentsOf: leftMenuViewControllersFromCode)
+    
     let rightMenuViewControllerNameArray: [String] = ["DPSettingsViewController"]
     let rightMenuViewControllers: [DPBaseEmbedViewController] = UIViewController.generateViewControllersFrom(viewControllerNameArray: rightMenuViewControllerNameArray, storyboardName: "Main", bundle: nil) as! [DPBaseEmbedViewController]
     
-    DPSlideMenuManager.shared.config(leftContentEmbedViewControllers: leftMenuViewControllers,
-                                     rightContentEmbedViewControllers: rightMenuViewControllers)
-    
     let homeViewController: DPHomeViewController? = self.storyboard?.instantiateViewController(withIdentifier: "DPHomeViewController") as? DPHomeViewController
-    let leftMenuViewController: DPLeftMenuViewController = DPLeftMenuViewController()
-    let rightMenuViewController: DPRightMenuViewController = DPRightMenuViewController()
-    
-    DPSlideMenuManager.shared.setup(leftMenuViewController: leftMenuViewController,
-                                    rightMenuViewController: rightMenuViewController,
+    DPSlideMenuManager.shared.setup(leftContentEmbedViewControllers: leftMenuViewControllers,
+                                    rightContentEmbedViewControllers: rightMenuViewControllers,
                                     centerContentViewController: homeViewController)
   }
   
